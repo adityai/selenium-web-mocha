aa2897d8c21da447e107dee192350123b2b04cc9
Aditya Inapurapu <adityaii@gmail.com>
2021-02-07 13:52:11 -0800

    First successful login test

---
+++ test/LoginTest.js
@@ -0,0 +1,37 @@
+const { Builder } = require('selenium-webdriver')
+// const path = require('path')`
+const assert = require('assert')
+
+function takeScreenshot(image, err) {
+  require('fs').writeFile('out.png', image, 'base64', function (err) {
+    console.log(err)
+  })
+}
+
+describe('Login', function () {
+  this.timeout(30000)
+  let driver
+
+  beforeEach(async function () {
+    driver = await new Builder().forBrowser('chrome').build()
+  })
+
+  afterEach(async function () {
+    if (this.currentTest.state == 'failed') {
+      driver.takeScreenshot().then(function (data) {
+        takeScreenshot(data, 'failed.png')
+      })
+    }
+    await driver.quit()
+  })
+
+  it('with in-valid credentials', async function () {
+    await driver.get('https://github.com/login')
+    await driver.findElement({ id: "login_field" }).sendKeys("incorrect-username")
+    await driver.findElement({ id: "password" }).sendKeys("tester")
+    await driver.findElement({ xpath: '//*[@id="login"]/div[4]/form/input[14]' }).click()
+    assert(await driver.findElement({ xpath: '//*[@id="js-flash-container"]/div' }).isDisplayed())
+
+  })
+})
+
